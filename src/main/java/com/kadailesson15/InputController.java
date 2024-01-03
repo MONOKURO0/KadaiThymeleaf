4b897dbcf503cfa7ae2c33773b6c9daab89b7556
MONOKURO0 <kurousagi-kerotan@live.jp>
2024-01-03 15:41:43 +0900

    不要なimportの除去

--- src/main/java/com/kadailesson15/InputController.java
+++ src/main/java/com/kadailesson15/InputController.java
@@ -2,10 +2,7 @@ package com.kadailesson15;
 
 import org.springframework.stereotype.Controller;
 
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class InputController {
